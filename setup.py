a4797ee991a822eb558cf8c3e9e99d1d807da5d4
lucho715 <lucho715@gmail.com>
2025-10-18 21:31:10 +0200

    0.0.3

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup (
+    name = 'Generative AI Medical Chat Bot Project',
+    version = '0.0.0',
+    author = 'Lucho Yang',
+    author_email = 'lucho715@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
